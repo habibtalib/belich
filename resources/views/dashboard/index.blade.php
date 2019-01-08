1bca644a7a743e9affbd34119b33a30992959cad
daguilarm <damian.aguilarm@gmail.com>
2019-01-08 10:43:44 +0100

    updated

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,10 +1,10 @@
 @extends('belich::layout')
 
-<table>
-    <thead>
-        <tr>
+<table class="m-4 text-sm">
+    <thead class="uppercase text-left text-grey-darker">
+        <tr class="border-t border-b border-solid border-grey-light bg-grey-lighter">
             @foreach($request['fields'] as $label => $attribute)
-                <th>{{ $label }}</th>
+                <th class="py-4 px-6">{{ $label }}</th>
             @endforeach
         </tr>
     </thead>
@@ -12,7 +12,7 @@
         @foreach($request['data'] as $data)
             <tr>
                 @foreach($request['fields'] as $label => $attribute)
-                    <td>{{ $data->{$attribute} }}</td>
+                    <td class="py-4 px-6 border-b border-solid border-grey-light">{{ $data->{$attribute} }}</td>
                 @endforeach
             </tr>
         @endforeach
