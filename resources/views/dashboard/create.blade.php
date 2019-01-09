09ea5937a8da8da126d48f5e8b7c8d24c483a3c9
daguilarm <damian.aguilarm@gmail.com>
2019-01-09 12:34:59 +0100

    updated

--- resources/views/dashboard/create.blade.php
+++ resources/views/dashboard/create.blade.php
@@ -1,6 +1,7 @@
 @extends('belich::layout')
 
-<form class="form">
+<form name="form-create" id="form-create" method="POST" action="">
+    @csrf
     @foreach($request['fields'] as $value)
         @includeIf('belich::fields.' . $value->type, ['request' => $value])
     @endforeach
--- resources/views/fields/components/inlineForm.blade.php
+++ resources/views/fields/components/inlineForm.blade.php
@@ -1,8 +1,8 @@
-<div>
-    <div class="w-1/3">
+<div class="form-group">
+    <div class="form-inline-label">
         <label>{{ $label }}</label>
     </div>
-    <div class="w-2/3">
+    <div class="form-inline-field">
         {{ $field }}
     </div>
 </div>
