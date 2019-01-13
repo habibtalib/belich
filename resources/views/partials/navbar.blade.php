39a748ed5de8d6accedf8c56bb63edf7b90fb729
daguilarm <damian.aguilarm@gmail.com>
2019-01-13 19:21:53 +0100

    url updated

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar">
   <div class="navbar-brand">
-        <a href="#">Tailwind</a>
+        <a href="{{ config('belich.url') }}">Belich Dashboard</a>
         <button class="navbar-button">
             <svg id="navbar-collapse-button-open" class="collapse-button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
             <svg id="navbar-collapse-button-close" class="collapse-button hidden" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M10 8.586L2.929 1.515 1.515 2.929 8.586 10l-7.071 7.071 1.414 1.414L10 11.414l7.071 7.071 1.414-1.414L11.414 10l7.071-7.071-1.414-1.414L10 8.586z"/></svg>
