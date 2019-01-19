8c4fe8fe5c61db7fab93d0035a80114bc90bd48c
daguilarm <damian.aguilarm@gmail.com>
2019-01-19 14:59:59 +0100

    Action added

--- resources/lang/en/belich.php
+++ resources/lang/en/belich.php
@@ -1,6 +1,12 @@
 <?php
 
 return [
+    'actions' => [
+        'create' => 'New',
+        'edit'   => 'Edit',
+        'index'  => 'List of',
+        'show'   => 'Information',
+    ],
     'navigation' => [
         'home' => 'Home',
     ],
