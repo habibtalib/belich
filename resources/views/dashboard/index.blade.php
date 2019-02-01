7c6f2c0aa201be971d384f521fb19d630760bd64
daguilarm <damian.aguilarm@gmail.com>
2019-02-01 18:29:01 +0100

    updated

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -4,9 +4,22 @@
     {{-- Breadcrumbs --}}
     @include('belich::partials.breadcrumbs')
 
-    {{-- Table --}}
+    {{-- Search --}}
+    <div class="table-search flex items-center">
+        <div class="icon-search">
+            <input type="text" name="_search" id="_search" placeholder="search..." onkeydown="showResetSearch()">
+            <span class="hidden" id="icon-search-reset">
+                <i class="fas fa-times-circle text-grey cursor-pointer" onclick="resetSearch()"></i>
+            </span>
+        </div>
+        <div class="flex w-full justify-end items-center"></div>
+        <div>
+            <button class="btn btn-primary">Hellow</button>
+        </div>
+    </div>
+
+    {{-- Start / Table --}}
     <form name="form-index" id="form-index" method="POST" action="">
-        <div class="table-head rounded-t-lg h-1"></div>
         <table class="table">
             <thead>
                 <tr>
@@ -48,18 +61,29 @@
                 </tfoot>
             @endif
         </table>
-        <div class="table-footer rounded-b-lg h-1 mb-16 shadow-md"></div>
     </form>
+    {{-- End / Table --}}
+    {{-- Table footer --}}
+    <div class="table-footer rounded-b-lg h-1 mb-16 shadow-md"></div>
 @endsection
 
 @section('javascript')
-    <script type="text/javascript" language="javascript">
+    <script>
         function checkAll(formName, selector) {
             var checkboxes = document[formName].getElementsByTagName('input');
             for (var i=0; i < checkboxes.length; i++)  {
                 if (checkboxes[i].type == 'checkbox')   {
                     checkboxes[i].checked = (selector.checked === true) ? true : false;
                 }
+            }
+        }
+        function resetSearch() {
+            document.getElementById('_search').value = '';
+            document.getElementById('icon-search-reset').classList.add('hidden');
+        }
+        function showResetSearch() {
+            if(document.getElementById('_search').value.length > 0) {
+                return document.getElementById('icon-search-reset').classList.remove('hidden');
             }
         }
     </script>
