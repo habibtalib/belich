abe52e375c4357553f4517343e255d637074e13f
daguilarm <damian.aguilarm@gmail.com>
2019-02-11 15:43:44 +0100

    updated

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,42 +1,46 @@
 @extends('belich::layout')
 
 @section('content')
-    <div class="w-full max-w-xs">
-        <form method="POST" action="{{ route('login') }}" class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-            @csrf
-            <div class="mb-4">
-                <label class="block text-grey-darker text-sm font-bold mb-2" for="email">
-                    Email
-                </label>
-                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker leading-tight focus:outline-none focus:shadow-outline" id="email" name="email" type="text" placeholder="email@email.com">
-                @if(isset($errors) && $errors->has('email'))
-                    <span class="text-red text-xs italic" for="email">
-                        <strong>{{ $errors->first('email') }}</strong>
-                    </span>
-                @endif
+    <form method="POST" action="{{ route('login') }}">
+        @csrf
+        <div class="container mx-auto h-full flex justify-center items-center">
+            <div class="w-1/3">
+                <h1 class="font-hairline mb-6 text-center">Login to Belich</h1>
+                <div class="border-teal p-8 border-t-12 bg-white mb-6 rounded-lg shadow-lg">
+                    <div class="mb-4">
+                        <label class="font-bold text-grey-darker block mb-2">Email</label>
+                        <input type="text" name="email" class="block appearance-none w-full bg-white border border-grey-light hover:border-grey px-2 py-2 rounded shadow" placeholder="Your email">
+                        @if(isset($errors) && $errors->has('email'))
+                            <span class="text-red text-xs italic" for="email">
+                                <strong>{{ $errors->first('email') }}</strong>
+                            </span>
+                        @endif
+                    </div>
+
+                    <div class="mb-4">
+                        <label class="font-bold text-grey-darker block mb-2">Password</label>
+                        <input type="password" name="password" class="block appearance-none w-full bg-white border border-grey-light hover:border-grey px-2 py-2 rounded shadow" placeholder="Your Password">
+                        @if(isset($errors) && $errors->has('password'))
+                            <span class="text-red text-xs italic" role="alert">
+                                <strong>{{ $errors->first('password') }}</strong>
+                            </span>
+                        @endif
+                    </div>
+
+                    <div class="flex items-center justify-between">
+                        <button type="submit" class="bg-teal-dark hover:bg-teal text-white font-bold py-2 px-4 rounded">
+                            Login
+                        </button>
+                        <a class="no-underline inline-block align-baseline font-bold text-sm text-blue hover:text-blue-dark float-right" href="#">
+                            Forgot Password?
+                        </a>
+                    </div>
+
+                </div>
+                <div class="text-center">
+                    <p class="text-grey-dark text-sm">Don't have an account? <a href="#" class="no-underline text-blue font-bold">Create an Account</a>.</p>
+                </div>
             </div>
-            <div class="mb-6">
-                <label class="block text-grey-darker text-sm font-bold mb-2" for="password">
-                    Password
-                </label>
-                <input class="shadow appearance-none border border-red rounded w-full py-2 px-3 text-grey-darker mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" name="password" type="password" placeholder="******************">
-                @if(isset($errors) && $errors->has('password'))
-                    <span class="text-red text-xs italic" for="password">
-                        <strong>{{ $errors->first('password') }}</strong>
-                    </span>
-                @endif
-            </div>
-            <div class="flex items-center justify-between">
-                <button class="bg-blue hover:bg-blue-dark text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
-                    Sign In
-                </button>
-                <a class="inline-block align-baseline font-bold text-sm text-blue hover:text-blue-darker" href="#">
-                    Forgot Password?
-                </a>
-            </div>
-        </form>
-        <p class="text-center text-grey text-xs">
-            ©2019 Acme Corp. All rights reserved.
-        </p>
-    </div>
+        </div>
+    </form>
 @endsection
