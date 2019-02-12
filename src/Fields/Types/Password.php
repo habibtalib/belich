b93b2277544acec0153c6d33e67ca7c3d8bc11d7
daguilarm <damian.aguilarm@gmail.com>
2019-02-12 20:29:40 +0100

    New field password

---
+++ src/Fields/Types/Password.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Daguilarm\Belich\Fields\Types;
+
+use Daguilarm\Belich\Fields\Field;
+
+class Password extends Field {
+
+    /**
+     * Field type
+     *
+     * @var string
+     */
+    public $type = 'password';
+}
