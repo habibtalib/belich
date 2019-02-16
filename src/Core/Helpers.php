79b6956bd88b3fa3cf620d90d9982aac03f845bf
daguilarm <damian.aguilarm@gmail.com>
2019-02-16 12:00:52 +0100

    Added new trait to base

--- src/Core/Helpers.php
+++ src/Core/Helpers.php
@@ -2,11 +2,12 @@
 
 namespace Daguilarm\Belich\Core;
 
+use Daguilarm\Belich\Core\Traits\Models;
 use Daguilarm\Belich\Core\Traits\Operations;
 use Daguilarm\Belich\Core\Traits\Resource;
 use Daguilarm\Belich\Core\Traits\Route;
 use Daguilarm\Belich\Core\Traits\System;
 
 trait Helpers {
-    use Operations, Resource, Route, System;
+    use Models, Operations, Resource, Route, System;
 }
