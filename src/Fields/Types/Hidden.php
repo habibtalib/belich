404c7cf5e1afbe57532ae5b744afeee41f1a6582
daguilarm <damian.aguilarm@gmail.com>
2019-02-16 12:12:18 +0100

    New hidden field

---
+++ src/Fields/Types/Hidden.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Daguilarm\Belich\Fields\Types;
+
+use Daguilarm\Belich\Facades\Belich;
+use Daguilarm\Belich\Fields\Field;
+
+class Hidden extends Field {
+
+    /**
+     * Field type
+     *
+     * @var string
+     */
+    public $type = 'hidden';
+}
