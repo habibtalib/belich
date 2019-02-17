1e8376e492a98cd6000f922558d3611313a7cf39
daguilarm <damian.aguilarm@gmail.com>
2019-02-17 13:24:26 +0100

    forceDelete icon

--- resources/views/actions/default.blade.php
+++ resources/views/actions/default.blade.php
@@ -9,7 +9,7 @@
 @can('delete', $model)
     @isTrashed($model)
         <a href="{{ Belich::actionRoute('destroy', $model) }}" class="action">@actionIcon('redo')</a>
-        <a href="{{ Belich::actionRoute('destroy', $model) }}" class="action">@actionIcon('redo')</a>
+        <a href="{{ Belich::actionRoute('destroy', $model) }}" class="action bg-red-lightest rounded-full">@actionIcon('trash-alt')</a>
     @else
         <a href="{{ Belich::actionRoute('destroy', $model) }}" class="action">@actionIcon('trash')</a>
     @endif
