d8ad45741113f006867224a34685ca811319e0c5
daguilarm <damian.aguilarm@gmail.com>
2019-02-17 14:03:07 +0100

    new actions

--- resources/views/actions/default.blade.php
+++ resources/views/actions/default.blade.php
@@ -7,13 +7,17 @@
 @endcan
 
 @can('delete', $model)
+    {{-- If the model has been trashed --}}
     @isTrashed($model)
+        {{-- The user can restore trashed items --}}
         @can('restore', $model)
             <a href="{{ Belich::actionRoute('destroy', $model) }}" class="action">@actionIcon('redo')</a>
         @endcan
+        {{-- The user can force delete trashed items --}}
         @can('forceDelete', $model)
             <a href="{{ Belich::actionRoute('destroy', $model) }}" class="action bg-red-lightest rounded-full">@actionIcon('trash-alt')</a>
         @endcan
+    {{-- Just the regular deleting... --}}
     @else
         <a href="{{ Belich::actionRoute('destroy', $model) }}" class="action">@actionIcon('trash')</a>
     @endif
