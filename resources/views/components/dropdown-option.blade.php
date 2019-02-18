007f584b99f3422792dfceb2c7430de8b3ea7c34
daguilarm <damian.aguilarm@gmail.com>
2019-02-18 17:48:45 +0100

    refactoring

--- resources/views/components/dropdown-option.blade.php
+++ resources/views/components/dropdown-option.blade.php
@@ -2,17 +2,17 @@
 <div class="w-full mb-1">
     <div class="w-full p-4 bg-grey-lighter border-b border-grey-light">{!! icon($icon, $text, 'opacity-50') !!}</div>
     <div class="p-2 text-lg">
-        <select name="{{ $field }}" class="w-full h-10">
-            <option></option>
-            @foreach($options as $option)
-                @if(!empty($option))
+        @isset($options)
+            <select name="{{ $field }}" class="w-full h-10">
+                <option></option>
+                @foreach($options as $option)
                     @if(is_array($option) && count($option) <= 1)
                         <option value="{{ array_keys($option)[0] }}">{{ array_values($option)[0] }}</option>
                     @else
                         <option value="{{ $option }}">{{ $option }}</option>
                     @endif
-                @endif
-            @endforeach
-        </select>
+                @endforeach
+            </select>
+        @endisset
     </div>
 </div>
