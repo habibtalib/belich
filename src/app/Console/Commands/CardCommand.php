2d7a45e8bcfafecf1ae6f14a04df25c37064098a
daguilarm <damian.aguilarm@gmail.com>
2019-02-27 20:51:57 +0100

    cards are ready!

--- src/app/Console/Commands/CardCommand.php
+++ src/app/Console/Commands/CardCommand.php
@@ -53,16 +53,6 @@ class CardCommand extends BelichCommand
     }
 
     /**
-     * Get the stub view file
-     *
-     * @return string
-     */
-    protected function getStubView()
-    {
-        return __DIR__ . '/../../../stubs/card_view.stub';
-    }
-
-    /**
      * Set the stub destination
      *
      * @return string
@@ -70,16 +60,6 @@ class CardCommand extends BelichCommand
     protected function setStub($ext = 'stub')
     {
         return $this->path() . $this->argument('className') . '.' . $ext;
-    }
-
-    /**
-     * Set the stub view destination
-     *
-     * @return string
-     */
-    protected function setStubView($ext = 'stub')
-    {
-        return app_path('resources/views/vendor/belich/cards/' . $this->argument('className') . '.' . $ext);
     }
 
     /**
@@ -94,11 +74,6 @@ class CardCommand extends BelichCommand
         }
 
         $this->handleClass();
-
-        if($this->option('view')) {
-            //Second the view
-            $this->handleView();
-        }
     }
 
     /**
@@ -121,43 +96,12 @@ class CardCommand extends BelichCommand
         // Replacements
         $this->replace('d_class_b', $this->argument('className'), $this->setStub());
         $this->replace('d_key_b', Str::kebab($this->argument('className')), $this->setStub());
-        $this->replace('d_view_b', $this->classSnake(), $this->setStub());
+        $this->replace('d_view_b', Str::snake($this->argument('className')), $this->setStub());
 
         //Set the file
         (new Filesystem)->move(
             $this->setStub(),
             $this->setStub('php')
-        );
-    }
-
-    /**
-     * Handle the view
-     *
-     * @return void
-     */
-    protected function handleView()
-    {
-        $path = app_path('resources/views/vendor/belich/cards');
-
-        if(!File::exists($path)) {
-            File::makeDirectory($path);
-        }
-
-        //Copy the file to folder while keeping the .stub extension
-        (new Filesystem)->copy(
-            $this->getStubView(),
-            $this->setStubView()
-        );
-
-        // Replacements
-        $this->replace('d_class_b', $this->argument('className'), $this->setStubView());
-        $this->replace('d_key_b', Str::kebab($this->argument('className')), $this->setStubView());
-        $this->replace('d_view_b', $this->classSnake(), $this->setStubView());
-
-        //Set the file
-        (new Filesystem)->move(
-            $this->setStubView(),
-            $this->setStubView('php')
         );
     }
 }
