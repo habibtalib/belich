1ee544595605a420871b9dc3f3e5b917e899998b
daguilarm <damian.aguilarm@gmail.com>
2019-02-28 13:51:06 +0100

    removed innecesary code

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,6 +1,3 @@
-<div class="{{ $width ?? 'w-full' }} metrics p-2 overflow-hidden shadow bg-white border border-grey-lighter">
-    <h4 class="text-grey-darker mt-2 px-4 ml-2">{{ $header }}</h4>
-    <div class="h-full py-4 pr-4">
-        {{ $content }}
-    </div>
+<div class="{{ $width ?? 'w-full' }} p-8 overflow-hidden shadow bg-white border border-grey-lighter">
+    {{ $content }}
 </div>
