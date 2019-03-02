d2fc1a4dc35a4e4e3eb0a655e3fdb7cdc05a5bd3
daguilarm <damian.aguilarm@gmail.com>
2019-03-02 17:27:29 +0100

    message: working on it...

--- resources/views/fields/avatar.blade.php
+++ resources/views/fields/avatar.blade.php
@@ -1,20 +1,5 @@
 <belich::fields :label="$field->label">
     <slot name="field">
-        <p class="{{ $field->color }}" onclick="toggleCheckbox('{{ $field->id }}');">
-            <input
-                type="checkbox"
-                value="{{ $field->value ?? 0 }}"
-                {!! setAttribute($field, 'addClass', 'itoggle ' . $field->color) !!}
-                {!! $field->render !!}
-                {!! setAttribute($field, 'checked') !!}
-            >
-            <label for="{{ $field->id }}"></label>
-        </p>
-
-        @if($field->help)
-            <div class="help-text">{{ $field->help }}</div>
-        @endif
-
-        <p id="error-{{ $field->id }}" class="validation-error"></p>
+        This is a file field alias so... first I have to finish the file field...
     </slot>
 </belich::fields>
