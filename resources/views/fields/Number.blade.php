7574e99c9c54efca508301ccc93e2b9a15e5c6b9
daguilarm <damian.aguilarm@gmail.com>
2019-03-03 14:48:35 +0100

    new field numbers

---
+++ resources/views/fields/Number.blade.php
@@ -0,0 +1,19 @@
+<belich::fields :label="$field->label">
+    <slot name="field">
+        <input
+            {!! setAttribute($field, 'addClass') !!}
+            {!! setAttribute($field, 'type') !!}
+            {!! setAttribute($field, 'value') !!}
+            {!! setAttribute($field, 'min') !!}
+            {!! setAttribute($field, 'max') !!}
+            {!! setAttribute($field, 'step') !!}
+            {!! $field->render !!}
+        >
+
+        @if($field->help)
+            <div class="help-text">{{ $field->help }}</div>
+        @endif
+
+        <p id="error-{{ $field->id }}" class="validation-error"></p>
+    </slot>
+</belich::fields>
