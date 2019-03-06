c1de8608c8be8519201a3af8ed5becd681f6580d
daguilarm <damian.aguilarm@gmail.com>
2019-03-06 17:11:52 +0100

    comments

--- resources/views/partials/navigation/breadcrumbs.blade.php
+++ resources/views/partials/navigation/breadcrumbs.blade.php
@@ -1,10 +1,14 @@
+{{-- Customize your breadcrumbs --}}
 <nav class="w-full rounded-lg rounded-b-none shadow bg-white">
     <ul class="flex list-reset my-3 p-4 font-semibold">
         @foreach($request->breadcrumbs as $label => $url)
+            {{-- Links --}}
             @if($label && $url)
                 <li>
                     <a href="{{ $url }}" class="text-teal-dark font-medium underline">{{ $label }}</a>
                 </li>
+
+            {{-- Current --}}
             @else
                 <li class="text-grey-darker">{{ $label }}</li>
             @endif
