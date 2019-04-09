d443cf3ba09ff91f6320e490d7d2258804be9c4b
daguilarm <damian.aguilarm@gmail.com>
2019-04-09 17:11:18 +0200

    updating to tailwindcss v1

--- resources/views/fields/header.blade.php
+++ resources/views/fields/header.blade.php
@@ -1,5 +1,5 @@
-<div class="w-full items-center py-8 px-6 border-b border-grey-lighter shadow-md {{ $field->color ?? 'text-grey-dark' }}  {{ $field->size ?? 'text-base' }} {{ $field->background ?? 'bg-grey-lightest' }}">
+<div class="w-full items-center py-8 px-6 border-b border-gray-200 shadow-md {{ $field->color ?? 'text-gray-600' }}  {{ $field->size ?? 'text-base' }} {{ $field->background ?? 'bg-gray-100' }}">
     <div class="capitalize font-bold">
-        <i class="fas fa-{{ $field->icon ?? 'angle-double-right' }} text-grey-light"></i> {{ $field->label }}
+        <i class="fas fa-{{ $field->icon ?? 'angle-double-right' }} {{ $field->color }} opacity-75"></i> {{ $field->label }}
     </div>
 </div>
