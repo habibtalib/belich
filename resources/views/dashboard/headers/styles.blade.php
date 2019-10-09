2752c4c29ea1f8abcc7380c8ef80b39635ffb2e7
daguilarm <damian.aguilarm@gmail.com>
2019-10-09 18:23:09 +0200

    working with assets

--- resources/views/dashboard/headers/styles.blade.php
+++ resources/views/dashboard/headers/styles.blade.php
@@ -2,7 +2,11 @@
 <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,800,800i,900,900i" rel="stylesheet">
 
 {{-- Vendor --}}
-@mix('app.css')
+@if (App::environment('local'))
+    <link href="/vendor/belich/app.css?id={{rand(1,10000000000)}}" rel="stylesheet">
+@else
+    @mix('app.css')
+@endif
 
 {{-- Css metrics --}}
 @hasMetrics($request ?? null)
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -10,9 +10,6 @@
         {{-- Styles --}}
         @include('belich::dashboard.headers.styles')
 
-        {{-- Livewire --}}
-        @livewireAssets
-
         {{-- Add Font-awesome --}}
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css"
             integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ"
