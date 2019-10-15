2c5b502cf97b1d5e7e0a39ba02145fd3d4ab3bec
daguilarm <damian.aguilarm@gmail.com>
2019-10-15 18:31:22 +0200

    fixed buttons errors

--- resources/views/partials/buttons/exports.blade.php
+++ resources/views/partials/buttons/exports.blade.php
@@ -3,31 +3,33 @@
     {{-- Set button icon --}}
     @icon('download', '', 'opacity-100')
 
-    {{-- Start with form --}}
-    <form method="POST" action="{{ route('dashboard.exports.download') }}" name="belich-form-exports" id="belich-form-exports" dusk="dusk-form-exports" class="btn-dropdown-content right-0 rounded-lg border border-gray-500 shadow text-gray-600 text-left bg-white">
-        @csrf
-        <input type="hidden" name="resource_model" value="{{ Belich::getModelPath() }}">
-        <input type="hidden" id="exports_selected" name="exports_selected" value="">
+    <div class="btn-dropdown-content">
+        {{-- Start with form --}}
+        <form method="POST" action="{{ route('dashboard.exports.download') }}" name="belich-form-exports" id="belich-form-exports" dusk="dusk-form-exports" class="btn-dropdown-content-item rounded-lg border border-gray-500 shadow text-gray-600 text-left bg-white">
+            @csrf
+            <input type="hidden" name="resource_model" value="{{ Belich::getModelPath() }}">
+            <input type="hidden" id="exports_selected" name="exports_selected" value="">
 
-        {{-- Export format component --}}
-        <belich::options field="format" css="rounded-t-lg" required="true" :text="trans('belich::default.format')">
-            <slot name="options">
-                <option value="xls">Excel 2003</option>
-                <option value="xlsx">Excel 2007</option>
-                <option value="csv">CSV</option>
-            </slot>
-        </belich::options>
+            {{-- Export format component --}}
+            <belich::options field="format" css="rounded-t-lg" required="true" :text="trans('belich::default.format')">
+                <slot name="options">
+                    <option value="xls">Excel 2003</option>
+                    <option value="xlsx">Excel 2007</option>
+                    <option value="csv">CSV</option>
+                </slot>
+            </belich::options>
 
-        {{-- Export quantity component --}}
-        <belich::options field="quantity" css="rounded-t-lg" required="true" :text="trans('belich::default.items')">
-            <slot name="options">
-                <option value="all">@lang('belich::default.all')</option>
-                <option value="selected">@lang('belich::default.selected')</option>
-            </slot>
-        </belich::options>
+            {{-- Export quantity component --}}
+            <belich::options field="quantity" required="true" :text="trans('belich::default.items')">
+                <slot name="options">
+                    <option value="all">@lang('belich::default.all')</option>
+                    <option value="selected">@lang('belich::default.selected')</option>
+                </slot>
+            </belich::options>
 
-        <div class="float-right p-2 mb-2">
-            <button type="submit" class="btn btn-default" onclick="addCheckboxesToField('exports_selected');">{!! icon('download', trans('belich::buttons.base.download')) !!}</button>
-        </div>
-    </form>
+            <div class="w-full flex flex-row-reverse p-2 mb-2">
+                <button type="submit" class="btn btn-default" onclick="addCheckboxesToField('exports_selected');">{!! icon('download', trans('belich::buttons.base.download')) !!}</button>
+            </div>
+        </form>
+    </div>
 </div>
