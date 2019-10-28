59ce07980ea9b58ea2e8a32c2f5db4f07221b16f
daguilarm <damian.aguilarm@gmail.com>
2019-10-28 08:15:00 +0100

    Added dusk support

--- resources/views/actions/default.blade.php
+++ resources/views/actions/default.blade.php
@@ -2,25 +2,25 @@
 @hasSoftdeletedResults($model)
     {{-- The user can restore trashed items --}}
     @can('restore', $model)
-        <a href="{{ Belich::actionRoute('restore', $model) }}" class="text-gray text-lg p-1 hover:text-gray-600">@actionIcon('redo')</a>
+        <a href="{{ Belich::actionRoute('restore', $model) }}" dusk="action-restore-{{ $model->id}}" class="text-gray text-lg p-1 hover:text-gray-600">@actionIcon('redo')</a>
     @endcan
     {{-- The user can force delete trashed items --}}
     @can('forceDelete', $model)
-        <a href="{{ Belich::actionRoute('forceDelete', $model) }}" class="text-gray-500 text-lg p-1 hover:text-gray-600 bg-red-lightest rounded-full">@actionIcon('trash-alt')</a>
+        <a href="{{ Belich::actionRoute('forceDelete', $model) }}" dusk="action-forceDelete-{{ $model->id}}" class="text-gray-500 text-lg p-1 hover:text-gray-600 bg-red-lightest rounded-full">@actionIcon('trash-alt')</a>
     @endcan
 
 {{-- Regular model results --}}
 @else
     @can('view', $model)
-        <a href="{{ Belich::actionRoute('show', $model) }}" class="text-gray-500 text-lg p-1 hover:text-gray-600">@actionIcon('eye')</a>
+        <a href="{{ Belich::actionRoute('show', $model) }}" dusk="action-view-{{ $model->id}}" class="text-gray-500 text-lg p-1 hover:text-gray-600">@actionIcon('eye')</a>
     @endcan
 
     @can('update', $model)
-        <a href="{{ Belich::actionRoute('edit', $model) }}" class="text-gray-500 text-lg p-1 hover:text-gray-600">@actionIcon('edit')</a>
+        <a href="{{ Belich::actionRoute('edit', $model) }}" dusk="action-update-{{ $model->id}}" class="text-gray-500 text-lg p-1 hover:text-gray-600">@actionIcon('edit')</a>
     @endcan
 
     @can('delete', $model)
-        <a href="#modal-item-delete" data-id="{{ $model->id }}" class="text-gray-500 text-lg p-1 hover:text-gray-600" onclick="deleteField('form-item-delete', '{{ Belich::actionRoute('destroy', $model) }}');">
+        <a href="#modal-item-delete" data-id="{{ $model->id }}" dusk="action-delete-{{ $model->id}}" class="text-gray-500 text-lg p-1 hover:text-gray-600" onclick="deleteField('form-item-delete', '{{ Belich::actionRoute('destroy', $model) }}');">
             @actionIcon('trash')
         </a>
     @endcan
