9f60ebecdf63495a3f8a42f0c53e5e62be5eccb6
daguilarm <damian.aguilarm@gmail.com>
2019-11-12 19:08:25 +0100

    Fixed error with helper

--- resources/views/partials/buttons/exports.blade.php
+++ resources/views/partials/buttons/exports.blade.php
@@ -28,7 +28,7 @@
             </belich::options>
 
             <div class="w-full flex flex-row-reverse p-2 mb-2">
-                <button type="submit" class="btn btn-default" onclick="addCheckboxesToField('exports_selected');">{!! icon('download', trans('belich::buttons.base.download')) !!}</button>
+                <button type="submit" class="btn btn-default" onclick="addCheckboxesToField('exports_selected');">{!! Helper::icon('download', trans('belich::buttons.base.download')) !!}</button>
             </div>
         </form>
     </div>
