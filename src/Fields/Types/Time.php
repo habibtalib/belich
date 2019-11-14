0e2d3aaf0c4187b935d417a9cf1d0e05bc3945f6
daguilarm <damian.aguilarm@gmail.com>
2019-11-14 22:15:24 +0100

    New fields: time and url

---
+++ src/Fields/Types/Time.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Daguilarm\Belich\Fields\Types;
+
+use Daguilarm\Belich\Fields\Types\Text;
+
+final class Time extends Text
+{
+    /**
+     * @var string
+     */
+    public $type = 'time';
+
+    /**
+     * @var string
+     */
+    public $max;
+
+    /**
+     * @var string
+     */
+    public $min;
+
+    /**
+     * @var int|float
+     */
+    public $step;
+
+    /**
+     * Set min value
+     *
+     * @param string $min
+     *
+     * @return self
+     */
+    public function min(string $min): self
+    {
+        $this->min = $min;
+
+        return $this;
+    }
+
+    /**
+     * Set max value
+     *
+     * @param string $max
+     *
+     * @return self
+     */
+    public function max(string $max): self
+    {
+        $this->max = $max;
+
+        return $this;
+    }
+
+    /**
+     * Set step value
+     *
+     * @param string $step
+     *
+     * @return self
+     */
+    public function step(string $step): self
+    {
+        $this->step = $step;
+
+        return $this;
+    }
+}
---
+++ src/Fields/Types/Url.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Daguilarm\Belich\Fields\Types;
+
+use Daguilarm\Belich\Fields\Types\Text;
+
+final class Url extends Text
+{
+    /**
+     * @var string
+     */
+    public $type = 'url';
+}
