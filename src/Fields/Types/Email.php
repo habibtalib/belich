6369eee1796c07ba0963bbbb007794ba02dd4cf4
daguilarm <damian.aguilarm@gmail.com>
2019-11-14 12:42:40 +0100

    New email field

---
+++ src/Fields/Types/Email.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Daguilarm\Belich\Fields\Types;
+
+use Daguilarm\Belich\Contracts\FieldMultipleContract;
+use Daguilarm\Belich\Fields\Field;
+
+final class Email extends Text implements FieldMultipleContract
+{
+    /**
+     * @var string
+     */
+    public $type = 'email';
+
+    /**
+     * @var bool
+     */
+    public $multiple = false;
+
+    /**
+     * Create a new field.
+     *
+     * @param  string|null  $name
+     * @param  string|null  $attribute
+     *
+     * @return  void
+     */
+    public function __construct($name = null, $attribute = null)
+    {
+        parent::__construct($name, $attribute);
+
+        //Cast the field as string
+        $this->toString();
+    }
+
+    /**
+     * Allow multiple emails (coma separate)
+     *
+     * @return self
+     */
+    public function multiple(): self
+    {
+        $this->multiple = true;
+
+        return $this;
+    }
+}
