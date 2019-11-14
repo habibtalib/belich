1f04827308ba5e52ec9fd27c18e5f469267b2107
daguilarm <damian.aguilarm@gmail.com>
2019-11-14 12:43:05 +0100

    Added contract for multiple method: only in file or email fields

---
+++ src/Contracts/FieldMultipleContract.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Daguilarm\Belich\Contracts;
+
+interface FieldMultipleContract {
+
+    /**
+     * Add multiple attribute to fields: file or email
+     */
+    public function multiple();
+}
