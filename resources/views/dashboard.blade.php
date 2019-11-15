47d5e124d3b2145a820e989c02e938c118297805
daguilarm <damian.aguilarm@gmail.com>
2019-11-15 13:30:24 +0100

    Limit to 5

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,7 +7,7 @@
         <belich::calendar id="tool-calendar" width="w-1/3"></belich::calendar>
 
         {{-- Model to table --}}
-        <belich::model :columns="['id', 'name', 'email']" :model="app(\App\User::class)" id="tool-model" width="w-2/3" limit="10" ></belich::model>
+        <belich::model :columns="['id', 'name', 'email']" :model="app(\App\User::class)" id="tool-model" width="w-2/3" limit="5" ></belich::model>
 
     </div>
 @endsection
