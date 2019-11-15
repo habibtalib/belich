fda2e6710adfb951c6554666e37c9e27a1656d0e
daguilarm <damian.aguilarm@gmail.com>
2019-11-15 10:29:12 +0100

    Moved to components/tools

---
+++ resources/views/components/tools/calendar.blade.php
@@ -0,0 +1,18 @@
+{{-- Calendar --}}
+<div class="w-32 flex-none text-center">
+    <div class="block overflow-hidden shadow-md rounded-t">
+        <div class="bg-blue-500 text-white text-xl py-2">
+            {{ now()->englishMonth }}
+        </div>
+        <div class="pt-1">
+            <span class="text-5xl font-bold leading-tight">
+                {{ now()->day }}
+            </span>
+        </div>
+        <div class="text-center border-white py-2 mb-1">
+            <span class="text-sm">
+                {{ now()->englishDayOfWeek }}
+            </span>
+        </div>
+    </div>
+</div>
