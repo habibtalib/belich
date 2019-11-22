0f9dabfb94ef80aef122ddb56da2ed16bf1e0f9c
daguilarm <damian.aguilarm@gmail.com>
2019-11-22 17:01:13 +0100

    Refactoring

--- src/app/Http/Controllers/Traits/Relationable.php
+++ src/app/Http/Controllers/Traits/Relationable.php
@@ -12,16 +12,43 @@ trait Relationable
      * @param object $model
      * @param Symfony\Component\HttpFoundation\ParameterBag $request
      *
-     * @return \Illuminate\Http\RedirectResponse
+     * @return
      */
     protected function updateRelationship(object $model, ParameterBag $request)
     {
+        return $this->handleRelationship($model, $request, 'update');
+    }
+
+    /**
+     * Redirect back with message
+     *
+     * @param object $model
+     * @param Symfony\Component\HttpFoundation\ParameterBag $request
+     *
+     * @return
+     */
+    protected function createRelationship(object $model, ParameterBag $request)
+    {
+        return $this->handleRelationship($model, $request, 'create');
+    }
+
+    /**
+     * Handle relationship
+     *
+     * @param object $model
+     * @param Symfony\Component\HttpFoundation\ParameterBag $request
+     * @param string $type
+     *
+     * @return
+     */
+    private function handleRelationship(object $model, ParameterBag $request, string $type)
+    {
         return collect($request)
-            ->each(function ($value, $field) use ($model): void {
+            ->each(function ($value, $field) use ($model, $type): void {
                 // Search for a relationship
                 if (method_exists($model , $field) && is_array($value) && count($value) > 0) {
                     // Update the relationship
-                    $model->$field()->update($value);
+                    $model->{$field}()->{$type}($value);
                 }
             });
     }
