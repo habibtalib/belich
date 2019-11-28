ccbbf695852aa86e2ffb064668ce70eccc04ee99
daguilarm <damian.aguilarm@gmail.com>
2019-11-28 10:52:48 +0100

    Conditional fields

---
+++ src/Fields/Types/Conditional.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Daguilarm\Belich\Fields\Types;
+
+use Daguilarm\Belich\Facades\Helper;
+use Daguilarm\Belich\Fields\Types\Header;
+use Illuminate\Support\Collection;
+
+class Conditional
+{
+    /**
+     * Create a new panel
+     *
+     * @param  \Closure  $fields
+     * @param  string  $dependsOn
+     * @param  mixed  $dependensOnValue
+     *
+     * @return array
+     */
+    public static function make(callable $fields, string $dependsOn, $dependensOnValue): array
+    {
+        // Get all the fields
+        $fields = static::getFields($field);
+
+        return static::createFields($fields, $dependsOn, $dependensOnValue);
+    }
+
+    /**
+     * Resolve fields in callback
+     *
+     * @param  string  $name
+     * @param  \Closure  $fields
+     *
+     * @return Illuminate\Support\Collection
+     */
+    protected static function getFields(callable $fields): Collection
+    {
+        $listOfFields = call_user_func($fields);
+
+        return collect($listOfFields);
+    }
+
+    /**
+     * Resolve fields in callback
+     *
+     * @param Illuminate\Support\Collection $fields
+     * @param  string  $dependsOn
+     * @param  mixed  $dependensOnValue
+     *
+     * @return array
+     */
+    protected static function createFields(Collection $fields, string $dependsOn, $dependensOnValue): array
+    {
+        return $fields
+            ->map(static function ($field) use ($dependsOn, $dependensOnValue) {
+                return $field;
+            })
+            ->toArray();
+    }
+}
