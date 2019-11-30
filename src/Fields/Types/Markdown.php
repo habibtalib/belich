009d415a3652a6940aa3d51ea1b9c28a37942b66
daguilarm <damian.aguilarm@gmail.com>
2019-11-30 11:17:45 +0100

    New field markdown

--- src/Fields/Types/Markdown.php
+++ src/Fields/Types/Markdown.php
@@ -15,4 +15,21 @@ final class Markdown extends TextArea
      * @var string
      */
     public $markdown = true;
+
+    /**
+     * Create a new field
+     *
+     * @param  string  $name
+     * @param  string|null  $attribute
+     *
+     * @return  void
+     */
+    public function __construct($label, $attribute = null)
+    {
+        //Set the values
+        parent::__construct($label, $attribute);
+
+        //Cast the field as string
+        $this->hideFromIndex();
+    }
 }
