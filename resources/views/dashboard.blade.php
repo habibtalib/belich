9846ad5de510273be245db05ac8874fbf64092f4
daguilarm <damian.aguilarm@gmail.com>
2019-11-30 08:47:10 +0100

    ID attribute added (to avoid test error)

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,10 +4,10 @@
     <div class="flex flex-wrap my-8 mx-6 p-4 rounded bg-white {{ config('belich.navbar') === 'top' ? 'shadow-md' : '' }}">
 
         {{-- Calendar --}}
-        <belich::calendar width="w-1/3"></belich::calendar>
+        <belich::calendar id="tool-calendar" width="w-1/3"></belich::calendar>
 
         {{-- Model to table --}}
-        <belich::model :columns="['id', 'name', 'email']" model="\App\User" width="w-2/3" limit="5" ></belich::model>
+        <belich::model id="tool-model" :columns="['id', 'name', 'email']" model="\App\User" width="w-2/3" limit="5" ></belich::model>
 
     </div>
 @endsection
