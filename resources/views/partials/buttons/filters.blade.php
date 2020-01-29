b5cf48f07dafb95b819de210c96d14a4048e2dd9
daguilarm <damian.aguilarm@gmail.com>
2020-01-29 19:30:53 +0100

    Fixed error in livesearch

--- resources/views/partials/buttons/filters.blade.php
+++ resources/views/partials/buttons/filters.blade.php
@@ -2,7 +2,7 @@
 @if($filters = Belich::filters($request))
     <div id="filters-container" dusk="button-options-filters" class="btn btn-dropdown mr-2 bg-blue-300 rounded-lg text-white hover:bg-blue-500">
         {{-- Set button icon --}}
-        @icon('filter', '', 'opacity-100')
+        @icon('b-filter', '', 'opacity-100')
 
         {{-- btn-dropdown --}}
         <div class="btn-dropdown-content">
@@ -12,11 +12,11 @@
             </div>
         </div>
     </div>
-
-    {{-- Search state values --}}
-    <input type="hidden" id="live_search_query" value="{{ $search['query'] ?? '' }}">
-    <input type="hidden" id="live_search_page" value="{{ $search['page'] ?? '' }}">
-    <input type="hidden" id="live_search_order" value="{{ $search['orderBy'] ?? '' }}">
-    <input type="hidden" id="live_search_direction" value="{{ $search['direction'] ?? '' }}">
-    <input type="hidden" id="live_search_filters" value="{{ $search['filters'] ?? '' }}">
 @endif
+
+{{-- Search state values --}}
+<input type="hidden" id="live_search_query" value="{{ $search['query'] ?? '' }}">
+<input type="hidden" id="live_search_page" value="{{ $search['page'] ?? '' }}">
+<input type="hidden" id="live_search_order" value="{{ $search['orderBy'] ?? '' }}">
+<input type="hidden" id="live_search_direction" value="{{ $search['direction'] ?? '' }}">
+<input type="hidden" id="live_search_filters" value="{{ $search['filters'] ?? '' }}">
