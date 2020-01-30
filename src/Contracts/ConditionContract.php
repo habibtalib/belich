24454c26a56bf93e4aa09f0ca2f26ff681e70126
daguilarm <damian.aguilarm@gmail.com>
2020-01-30 15:01:41 +0100

    Updated to php7.4

--- src/Contracts/ConditionContract.php
+++ src/Contracts/ConditionContract.php
@@ -1,13 +1,13 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Daguilarm\Belich\Contracts;
 
 interface ConditionContract
 {
     /**
      * Resolve condition
-     *
-     * @return bool
      */
     public function condition(): bool;
 }
--- src/Contracts/CrudContract.php
+++ src/Contracts/CrudContract.php
@@ -1,42 +1,28 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Daguilarm\Belich\Contracts;
 
 interface CrudContract
 {
     /**
      * Resolve value for index
-     *
-     * @param  object $field
-     * @param  object $data
-     *
-     * @return string|null
      */
     public function index(object $field, ?object $data = null): ?string;
 
     /**
      * Resolve value for create
-     *
-     * @param  object $data
-     *
-     * @return string|null
      */
     public function create(object $field, ?object $data = null): ?string;
 
     /**
      * Resolve value for edit
-     *
-     * @param  object $data
-     *
-     * @return string|null
      */
     public function edit(object $field, ?object $data = null): ?string;
 
     /**
      * Resolve value for show
-     *
-     * @param  object $field
-     * @param  object|null $data
      */
-    public function show(object $field, ?object $data = null);
+    public function show(object $field, ?object $data = null): ?string;
 }
